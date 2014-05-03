4b20291fcbc60aa7b7de895aee30bac3c85a4185
Kamil Smuga <smugakamil@gmail.com>
2014-05-03 14:02:39 +0100

    couch test

--- start.py
+++ start.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'flexi.note says hello!'
+
+@app.route('/couch')
+def test():
+    couch = couchdb.Server()
+    db = couch.create('test')
+    print db
+    db['foo'] = {'bar' : 'test'}
+    return db['foo']
