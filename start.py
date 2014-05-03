5c59dad41c8b946ce887ab1c075e6edeef5e32f9
Kamil Smuga <smugakamil@gmail.com>
2014-05-03 14:42:47 +0100

    couch links and auth

--- start.py
+++ start.py
@@ -5,6 +5,10 @@ import logging
 
 app = Flask(__name__)
 logging.basicConfig(filename='debug.log', level=logging.INFO)
+KUROBASE_BUCKET = os.environ['KUROBASE_BUCKET']
+KUROBASE_PASS = os.environ['KUROBASE_PASS']
+KUROBASE_SERVER = os.environ['KUROBASE_SERVER']
+KUROBASE_URL = os.environ['KUROBASE_URL']
 
 @app.route('/')
 def hello():
@@ -15,11 +19,16 @@ def hello():
 def test():
     print 'test'
     logging.info("i'm in /couch")
-    couch = couchdb.Server()
+    couch = couchdb.Server(url="http://"+KUROBASE_BUCKET+":"+KUROBASE_PASS+"@"+KUROBASE_URL)
+    print 'dupa'
     logging.info("couch server created")
     db = couch.create('test')
+    print '2 dupy'
     logging.info("db test created")
     logging.info(db)
     db['foo'] = {'bar' : 'test'}
     logging.info(db['foo'])
     return db['foo']
+
+if __name__ == '__main__':
+    app.run(debug=True)
